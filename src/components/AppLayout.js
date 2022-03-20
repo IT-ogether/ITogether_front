82aed9e6456f539a0e9eaed667e4e9a0b24a6e39
Hyevvy <hyeg0@naver.com>
2022-03-21 01:45:25 +0900

    feat: header 구현

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,7 +1,9 @@
 import Footer from './Footer';
+import Header from './Header';
 const AppLayout = ({ children }) => {
   return (
     <div>
+      <Header />
       {children}
       <Footer />
     </div>
