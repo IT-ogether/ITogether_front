58da2036c027ff872a9454ebed45f7391d9e31f8
KoDongE <bogo1357@hanmail.net>
2022-04-03 00:55:53 +0900

    feature/login : 로그인 리팩토링

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,13 @@
-import KakaoLogin from './KakaoLogin';
-import KakaoLogout from './KakaoLogout';
+import KakaoLoginOut from './KakaoLoginOut';
 
 //TODO : 추후 검색창 추가
 const Header = () => {
   return (
     <div className="Header">
       <div className="Header__logo">ITogether(로고)</div>
-      <div></div>
-      {localStorage.getItem('isLogin') === null ? (
-        <KakaoLogin />
-      ) : (
-        <KakaoLogout />
-      )}
+      <div>
+        <KakaoLoginOut />
+      </div>
     </div>
   );
 };
---
+++ src/components/KakaoLoginOut.js
@@ -0,0 +1,38 @@
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
+const KakaoLoginOut = () => {
+  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const [btnText, setBtnText] = useState(
+    localStorage.getItem('isLogin') === null ? 'LOGIN' : 'LOGOUT'
+  );
+  const longinOnClick = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
+  const logoutOnClick = async () => {
+    await window.Kakao.API.request({ url: '/v1/user/unlink' });
+    localStorage.removeItem('isLogin');
+    setBtnText((btnText) => 'LOGIN');
+  };
+
+  return (
+    <div>
+      <Button
+        variant="outlined"
+        onClick={() =>
+          localStorage.getItem('isLogin') === null
+            ? longinOnClick()
+            : logoutOnClick()
+        }
+      >
+        {btnText}
+      </Button>
+    </div>
+  );
+};
+
+export default KakaoLoginOut;
