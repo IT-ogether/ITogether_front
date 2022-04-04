d14489159924c7ad293d16811eaa20c9e66c581f
Hyevvy <hyeg0@naver.com>
2022-04-05 02:49:11 +0900

    design: detailinfo css수정

--- src/pages/DetailInfo.js
+++ src/pages/DetailInfo.js
@@ -2,52 +2,75 @@ import AppLayout from '../components/AppLayout';
 import HashTag from '../components/HashTag';
 import ToolTip from '../components/ToolTip';
 import { useParams } from 'react-router-dom';
-import { Box, Button } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper
+} from '@mui/material';
 import SpeechBubbleLeft from '../components/SpeechBubble/SpeechBubbleLeft';
 import SpeechBubbleRight from '../components/SpeechBubble/SpeechBubbleRight';
-// const club_temp = {
-//   id: 1,
-//   title: '프로그라피',
-//   siteUrl: 'https://prography.org/',
-//   logo: 'https://user-images.githubusercontent.com/72402747/159286868-9c8ae539-7fb9-4c67-87e9-cf32708143c6.png',
-//   recruitment_period: '3,4월',
-//   activity_period: '5개월',
-//   qualifications: ['대학생', '직장인'],
-//   fields: ['프론트엔드', '백엔드', '디자이너', 'ios', '안드로이드'],
-//   details: '활동비 10만원'
-// }
 
 const DetailInfo = () => {
   const { id, category } = useParams();
+
+  const data = {
+    information_id: 11,
+    information_title: 'SOPT',
+    site_url: 'http://sopt.org/wp/',
+    logo: 'http://sopt.org/wp/wp-content/uploads/2014/01/30_sopt_logo-1.png',
+    recruitment_period: '3월, 9월',
+    activity_period: '4개월',
+    qualifications: ['수도권 내 대학생'],
+    fields: ['프론트엔드', '백엔드', 'ios', '안드로이드', '디자이너'],
+    details: '디테일s'
+  };
 
   //TODO: 카테고리별 id를 주면 그에 해당하는 데이터 받아옴
 
   return (
     <AppLayout>
-      <Box>
-        <Box className="detailinfo__title">프로그라피</Box>
+      <div className="DetailInfo">
+        <div className="DetailInfo__header">
+          <div>
+            <img className="DetailInfo__header__img" src={data.logo} />
+          </div>
+          <div className="DetailInfo__header__title">
+            {data.information_title}
+          </div>
+        </div>
+
         <Button
           onClick={() => {
-            window.open('https://prography.org/', '_blank');
+            window.open(`${data.site_url}`, '_blank');
           }}
         >
           사이트 바로가기
         </Button>
-        <ToolTip titleText={'3월 - 4월'} hideText={'모집기간'} />
-        <HashTag
-          fields={['프론트엔드', '백엔드', '디자이너', 'ios', '안드로이드']}
+        <ToolTip
+          titleText={`모집기간 | ${data.recruitment_period}`}
+          hideText={'모집기간'}
         />
 
-        <Box>지원자격</Box>
-        <Box>
+        <ToolTip
+          titleText={`지원자격 | ${data.qualifications}`}
+          hideText={'지원자격'}
+        />
+        <HashTag fields={data.fields} />
+
+        <div className="DetailInfo__SpeechBubble">
           <SpeechBubbleLeft text={'모집일자는 변경될 수 있습니다 '} />
           <SpeechBubbleRight
-            text={
-              '모집이 시작되었을 때 알림이 받고 싶다면 ~ 채널을 추가해주세요!'
-            }
+            text={`모집이 시작되었을 때 알림이 받고 싶다면
+             ITogether 채널을 추가해주세요!`}
           />
-        </Box>
-      </Box>
+        </div>
+      </div>
     </AppLayout>
   );
 };
