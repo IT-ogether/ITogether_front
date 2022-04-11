00bbf7dc63824fd793e299cc308ee32d44e11657
KoDongE <bogo1357@hanmail.net>
2022-04-11 20:43:15 +0900

    refactor/Login

--- src/api/Auth.js
+++ src/api/Auth.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import qs from 'qs';
 import axios from 'axios';
-import { LocalPostOfficeRounded } from '@mui/icons-material';
 
 const Auth = () => {
   async function postCode(prop) {
@@ -14,21 +13,16 @@ const Auth = () => {
         process.env.REACT_APP_URL + '/oauth/kakao/login',
         authCode
       );
-      localStorage.setItem('nickName', response.data.nickName);
-      localStorage.setItem('email', response.data.email);
-      console.log(response.data.email + response.data.nickName);
+      localStorage.setItem('JWT_TOKEN', response.data.jwtAccessToken);
     } catch (error) {
       console.log(error);
     }
   }
 
-  const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_KEY;
   const navigate = useNavigate();
   useEffect(() => {
     const code = new URL(window.location.href).searchParams.get('code');
     navigate('/');
-
     postCode(code);
   }, []);
 
--- src/components/KakaoLoginOut.js
+++ src/components/KakaoLoginOut.js
@@ -1,7 +1,5 @@
 import Button from '@mui/material/Button';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 
 const KakaoLoginOut = () => {
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
@@ -9,29 +7,23 @@ const KakaoLoginOut = () => {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const [btnText, setBtnText] = useState(
-    localStorage.getItem('nickName') === null ? 'LOGIN' : 'LOGOUT'
+    localStorage.getItem('JWT_TOKEN') === null ? 'LOGIN' : 'LOGOUT'
   );
   const longinOnClick = () => {
     window.location.href = KAKAO_AUTH_URL;
   };
 
   const logoutOnClick = async () => {
+    localStorage.removeItem('JWT_TOKEN');
     setBtnText((btnText) => 'LOGIN');
   };
 
   return (
     <div>
-      <Link to="/profile">
-        <AssignmentIndIcon
-          style={{
-            fontSize: '40px'
-          }}
-        />
-      </Link>
       <Button
         variant="outlined"
         onClick={() =>
-          localStorage.getItem('nickName') === null
+          localStorage.getItem('JWT_TOKEN') === null
             ? longinOnClick()
             : logoutOnClick()
         }
