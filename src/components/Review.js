dec9291e9fdb10ecf9bbb5d9910713209b858667
Hyevvy <hyeg0@naver.com>
2022-04-19 12:45:16 +0900

    feat: 리뷰 사이트에 맞게 로고 출력

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,26 +1,27 @@
 const Review = ({ site, title, url }) => {
+  const getSrc = (site) => {
+    const idx = siteLogos.findIndex((siteLogo) => siteLogo.site === site);
+    return siteLogos[idx].logo;
+  };
+
   const siteLogos = [
     {
       site: 'naver',
-      logo: '../images/logo_naver'
+      logo: '/images/logo_naver.png'
     },
     {
       site: 'tistory',
-      logo: '../images/logo_tistory'
+      logo: '/images/logo_tistory.png'
     },
     {
       site: 'velog',
-      logo: '../images/logo_velog'
+      logo: '/images/logo_velog.png'
     }
   ];
   return (
     <div className="Review">
       <div>
-        <img
-          className="Review__img"
-          alt="ReviewSiteLogo"
-          src="/images/logo_naver.png"
-        />
+        <img className="Review__img" alt="ReviewSiteLogo" src={getSrc(site)} />
       </div>
       <div
         className="Review__title"
