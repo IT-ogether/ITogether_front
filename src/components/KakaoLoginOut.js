7d4b223fe81a18542c2771014105feb688b7f7b9
KoDongE <bogo1357@hanmail.net>
2022-05-29 18:17:24 +0900

    feature/bookMarks

--- src/components/KakaoLoginOut.js
+++ src/components/KakaoLoginOut.js
@@ -2,24 +2,23 @@ import Button from '@mui/material/Button';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { logout } from '../actions';
 
 const KakaoLoginOut = () => {
   const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
   const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-  const isLogged = useSelector((state) => state.isLogged);
   const dispatch = useDispatch();
-
+  const [isLogged, setIsLogged] = useState(false);
   const logoutClick = () => {
-    dispatch(logout());
     localStorage.removeItem('accessToken');
     localStorage.removeItem('refreshTokenIndex');
+    localStorage.removeItem('isLogged');
+    setIsLogged((state) => false);
   };
   useEffect(() => {
-    console.log('login');
-    console.log(isLogged);
+    if (localStorage.getItem('isLogged') != null)
+      setIsLogged((isLogged) => true);
   });
 
   return (
